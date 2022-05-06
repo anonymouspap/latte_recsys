73de3016f30a2e10a084e5e3f7d7acaa487a3f8e
anonymouspap <scalyvladimir@gmail.com>
2022-05-06 20:15:49 +0000

    added multivae implementation

--- main.py
+++ main.py
@@ -15,38 +15,51 @@ from scipy.linalg import sqrtm
 
 from Modules import read_amazon_data, full_preproccessing, make_prediction, model_evaluate
 from Random_MP import build_random_model, random_model_scoring, build_popularity_model, popularity_model_scoring
-from NormPureSVD_EASEr import build_svd_model, svd_model_scoring, easer, easer_scoring
+from NormPureSVD_EASEr import build_svd_model, svd_model_scoring, easer, easer_scoring, matrix_from_observations
 from CoFFee_LaTTe import full_pipeline, get_similarity_matrix
 import sys
+
+# +
+# Torch imports
+import torch.nn.functional as F
+import torch.nn as nn
+import torch
+
+from multivae.pytorch_models import MultiVAE
+# -
+
+# VAE imports
+from multivae.vae_utils import init_weights, vae_loss_fn, early_stopping
+from scipy.sparse import csr_matrix
 
 names = ["Movielens_1M", "Movielens_10M", "Video_Games", "CDs_and_Vinyl", "Electronics", "Video_Games_nf"]
 
 if len(sys.argv) > 1:
-   data_name = sys.argv[1]
-   assert data_name in names, f"Name of dataset must be one the following {names}"
+    data_name = sys.argv[1]
+    assert data_name in names, f"Name of dataset must be one the following {names}"
 else:
-   data_name = "Movielens_1M"
+    data_name = "Movielens_1M"
 
 print(f"Starting tuning models for dataset {data_name}.\n")
 
 if (data_name != "Electronics"):
-   q = 0.8
+    q = 0.8
 else:
-   q = 0.95
+    q = 0.95
 
 if data_name == "Movielens_1M":
-   data = None
-   name = "ml-1m.zip"
+    data = None
+    name = "ml-1m.zip"
 elif data_name == "Movielens_10M":
-   data = None
-   name = "ml-10m.zip"
+    data = None
+    name = "ml-10m.zip"
 elif data_name == "Video_Games_nf":
-   data = pd.read_csv("ratings_Video_Games.csv")
-   name = None
+    data = pd.read_csv("ratings_Video_Games.csv")
+    name = None
 else:
-   data = read_amazon_data(name = data_name)
-   data.rename(columns = {'reviewerID' : 'userid', 'asin' : 'movieid', "overall" : "rating", "unixReviewTime" : "timestamp"}, inplace = True) 
-   name = None
+    data = read_amazon_data(name = data_name)
+    data.rename(columns = {'reviewerID' : 'userid', 'asin' : 'movieid', "overall" : "rating", "unixReviewTime" : "timestamp"}, inplace = True) 
+    name = None
 
 
 training, testset_valid, holdout_valid, testset, holdout, data_description, data_index = full_preproccessing(data, name, q)
@@ -217,3 +230,283 @@ easer_scores = easer_scoring(easer_params, testset, data_description)
 downvote_seen_items(easer_scores, testset, data_description)
 
 _ = make_prediction(easer_scores, holdout, data_description, mode='Test')
+
+print("EASEr tuning ended.\n")
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+csr_training = matrix_from_observations(training, data_description)
+csr_testset_valid = matrix_from_observations(testset_valid, data_description)
+
+
+# +
+def train_step(model, optimizer, data, epoch, batch_size, anneal_cap, device='cpu'):
+
+    model.train()
+    running_loss = 0.0
+    global update_count
+    N = data.shape[0]
+    idxlist = list(range(N))
+    np.random.shuffle(idxlist)
+    training_steps = len(range(0, N, batch_size))
+
+    for batch_idx, start_idx in enumerate(range(0, N, batch_size)):
+
+        end_idx = min(start_idx + batch_size, N)
+        X_inp = data[idxlist[start_idx:end_idx]]
+        X_inp = torch.FloatTensor(X_inp.toarray()).to(device)
+
+        anneal = min(anneal_cap, update_count / total_anneal_steps)
+        update_count += 1
+
+        optimizer.zero_grad()
+        
+        X_out, mu, logvar = model(X_inp)
+        loss = vae_loss_fn(X_inp, X_out, mu, logvar, anneal)
+        train_step.anneal = anneal
+
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        avg_loss = running_loss / (batch_idx + 1)
+
+def eval_step(model, csr_data, data, holdout, batch_size, data_type="valid", holdout_description=None, device='cpu'):
+
+    model.eval()
+    running_loss = 0.0
+    eval_idxlist = list(range(csr_data.shape[0]))
+    eval_N = csr_data.shape[0]
+    eval_steps = len(range(0, eval_N, batch_size))
+    
+    n_users = data['userid'].nunique()
+
+    hr_list, mrr_list, C_list = [], [], []
+    mrr_pos_list, mrr_neg_list, cov_set = [], [], set()
+    hr_pos_list, hr_neg_list = [], [] 
+
+    users_pred = []
+    
+    with torch.no_grad():
+        for batch_idx, start_idx in enumerate(range(0, eval_N, batch_size)):
+
+            end_idx = min(start_idx + batch_size, eval_N)
+            X_tr = csr_data[eval_idxlist[start_idx:end_idx]]
+            X_tr_inp = torch.FloatTensor(X_tr.toarray()).to(device)
+        
+            X_out, mu, logvar = model(X_tr_inp)
+            loss = vae_loss_fn(X_tr_inp, X_out, mu, logvar, train_step.anneal)
+            
+            running_loss += loss.item()
+            avg_loss = running_loss / (batch_idx + 1)
+
+            # Exclude examples from training set
+            X_out = X_out.cpu().numpy()
+            X_out[X_tr.nonzero()] = -np.inf
+
+            recs = topn_recommendations(X_out, topn=10)
+
+            users_pred.append(recs)
+
+        user_pred = np.vstack(users_pred)[holdout.userid,:]
+                                
+    hr, hr_pos, hr_neg, mrr, mrr_pos, mrr_neg, cov, C = model_evaluate(user_pred, holdout, holdout_description, alpha=3, topn=10, dcg=False)
+
+    return avg_loss, hr, mrr, hr_pos, hr_neg, mrr_pos, mrr_neg, cov, C
+
+
+
+# +
+n_epochs = 200
+
+batch_size = 500
+# -
+
+dim_grid = [
+    [50, 150],
+    [100, 300],
+    [50, 300],
+    [200, 400],
+    [200, 600],
+    [400, 800],
+    [400, 1200]
+]
+
+print("MultiVAE tuning in progress...\n")
+
+# +
+c_list = []
+anneal_list = []
+
+n_items = training.movieid.nunique()
+
+for tuned_dims in tqdm(dim_grid):
+
+    model = MultiVAE(
+        p_dims=tuned_dims + [n_items],
+        q_dims=[n_items] + tuned_dims[::-1],
+        dropout_enc=[0.5, 0.],
+        dropout_dec=[0., 0.],
+    )
+
+    init_weights(model)
+
+    model.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+    ###########################################
+    # Finding best anneal factor for training #
+    ###########################################
+
+    anneal_cap = 1.0
+
+    training_steps = len(range(0, training.shape[0], batch_size))
+    try:
+        total_anneal_steps = (
+            training_steps * (n_epochs - int(n_epochs * 0.2))
+        ) / anneal_cap
+    except ZeroDivisionError:
+        assert (
+            constant_anneal
+        ), "if 'anneal_cap' is set to 0.0 'constant_anneal' must be set to 'True"
+
+    ################################
+    # Training with early stopping #
+    ################################
+
+    stop_step = 0
+    update_count = 0
+    stop = False
+
+    patience_val = 10 # steps to stop after plateau or decreasing metric
+    best_c = -np.inf
+
+    for epoch in range(n_epochs):
+
+        if stop:
+            break
+
+        train_step(model, optimizer, csr_training, epoch, batch_size, anneal_cap, device)
+
+        c_val = eval_step(model, csr_testset_valid, testset_valid, holdout_valid, batch_size, holdout_description=data_description, device=device)[-1]
+
+        best_c, stop_step, stop = early_stopping(c_val, best_c, stop_step, patience_val, score_fn='metric')
+
+    # Found best param    
+    anneal_cap = train_step.anneal
+    
+    training_steps = len(range(0, training.shape[0], batch_size))
+    try:
+        total_anneal_steps = (
+            training_steps * (n_epochs - int(n_epochs * 0.2))
+        ) / anneal_cap
+    except ZeroDivisionError:
+        assert (
+            constant_anneal
+        ), "if 'anneal_cap' is set to 0.0 'constant_anneal' must be set to 'True"
+
+    init_weights(model)
+
+    stop_step = 0
+    update_count = 0
+    stop = False
+
+    patience_val = 20 # steps to stop after plateau or decreasing metric
+    best_c = -np.inf
+
+    for epoch in range(n_epochs):
+        if stop:
+            break
+
+        train_step(model, optimizer, csr_training, epoch, batch_size, anneal_cap, device)
+
+        c_val = eval_step(model, csr_testset_valid, testset_valid, holdout_valid, batch_size, holdout_description=data_description, device=device)[-1]
+        
+        best_c, stop_step, stop = early_stopping(c_val, best_c, stop_step, patience_val, score_fn='metric')
+
+    c_list.append(best_c)
+    anneal_list.append(anneal_cap)
+
+best_id = np.argsort(c_list)[-1]
+
+best_params = {
+    'dims': dim_grid[best_id],
+    'anneal': anneal_list[best_id],
+}
+# -
+
+print('best found params:', best_params)
+
+dims = best_params['dims']
+
+# +
+model = MultiVAE(
+    p_dims=dims + [n_items],
+    q_dims=[n_items] + dims[::-1],
+    dropout_enc=[0.5, 0.],
+    dropout_dec=[0., 0.],
+)
+
+init_weights(model)
+
+model.to(device)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+# +
+anneal_cap = best_params['anneal']
+
+training_steps = len(range(0, training.shape[0], batch_size))
+try:
+    total_anneal_steps = (
+        training_steps * (n_epochs - int(n_epochs * 0.2))
+    ) / anneal_cap
+except ZeroDivisionError:
+    assert (
+        constant_anneal
+    ), "if 'anneal_cap' is set to 0.0 'constant_anneal' must be set to 'True"
+# -
+
+print("Training with best params in progress...")
+
+# +
+stop_step = 0
+update_count = 0
+stop = False
+
+patience_val = 20 # steps to stop after plateau or decreasing metric
+best_c = -np.inf
+
+for epoch in range(n_epochs):
+    
+    if stop:
+        break
+    
+    train_loss = train_step(model, optimizer, csr_training, epoch, batch_size, anneal_cap, device=device)
+    
+    val_loss, hr, mrr, _, _, _, _, _, c_val = eval_step(model, csr_testset_valid, testset_valid, holdout_valid, batch_size, holdout_description=data_description, device=device)
+
+    print("=" * 80)
+    print(
+        "| valid loss {:4.3f} | HR@10 {:4.3f} | MRR@10 {:4.3f} | "
+        "Matthew's {:4.3f}".format(val_loss, hr, mrr, c_val)
+    )
+    print("=" * 80)
+
+    best_c, stop_step, stop = early_stopping(c_val, best_c, stop_step, patience_val, score_fn='metric')    
+# -
+
+print("Evaluation on testset (MultiVAE) in progress...")
+
+csr_testset = matrix_from_observations(testset, data_description)
+
+_, hr, mrr, hr_pos, hr_neg, mrr_pos, mrr_neg, cov, C = eval_step(model, csr_testset, testset, holdout, batch_size, holdout_description=data_description, device=device)
+
+print("=" * 80)
+print(f"HR@10 = {hr:.4f}, MRR@10 = {mrr:.4f}, Coverage@10 = {cov:.4f}")
+print(f"HR_pos@10 = {hr_pos:.4f}, HR_neg@10 = {hr_neg:.4f}")
+print(f"MRR_pos@10 = {mrr_pos:.4f}, MRR_neg@10 = {mrr_neg:.4f}")
+print(f"Matthews@10 = {C:.4f}")
+print("=" * 80)
+
+print("MultiVAE tuning ended.\n")
